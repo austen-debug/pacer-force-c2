1734e2da5873bfbd3c59da4ab16d3389cfebb8b7
austen-debug <austentube@gmail.com>
2025-11-13 19:53:51 -0600

    Modify console log and apply temporary styles
    Updated console log message and added temporary styling.

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log("PACER FORCE C2 Loaded.");
+console.log("PACER FORCE C2 app.js loaded");
 
+// TEMPORARY SAFE VERSION
+document.body.style.background = "#111";
+document.body.style.color = "white";
+document.body.innerHTML += "<p>app.js is running successfully.</p>";
